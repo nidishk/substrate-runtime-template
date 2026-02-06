fb410f07374738bbe001974c47e27c89e2a26bab
nidishk <nidishkrishnan45@gmail.com>
2026-02-06 21:24:41 +0530

    feat: add full implementation with tests, remove build artifacts

--- target/debug/deps/substrate_runtime_template-27980442c9561246.d
+++
@@ -1,7 +0,0 @@
-/Users/nidishramakrishnan/Work/Cogni/portfolio-repos/2021/substrate-runtime-template/target/debug/deps/libsubstrate_runtime_template-27980442c9561246.rmeta: src/lib.rs
-
-/Users/nidishramakrishnan/Work/Cogni/portfolio-repos/2021/substrate-runtime-template/target/debug/deps/libsubstrate_runtime_template-27980442c9561246.rlib: src/lib.rs
-
-/Users/nidishramakrishnan/Work/Cogni/portfolio-repos/2021/substrate-runtime-template/target/debug/deps/substrate_runtime_template-27980442c9561246.d: src/lib.rs
-
-src/lib.rs:
